396686ae28da2040fab0c58ba80094c704f62e5d
Chananchida F <chida.fua@gmail.com>
2019-11-05 18:36:01 +0700

    Add views to save form

--- users/models.py
+++ users/models.py
@@ -10,7 +10,7 @@ class CustomUser(AbstractUser):
     emergency_contact = models.CharField(max_length=30, blank=True, null=True)
     gender = models.CharField(max_length=1)
     birthdate = models.DateField(blank=True, null=True)
-    
+
     def __str__(self):
         return self.email
 
--- users/views.py
+++ users/views.py
@@ -2,7 +2,9 @@ from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 
+from .models import CustomUser
 from .forms import CustomUserCreationForm
 # Create your views here.
 
@@ -15,15 +17,20 @@ def signup(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+            first_name = form.cleaned_data('first_name')
+            last_name = form.cleaned_data('last_name')
+            contact = form.cleaned_data('contact')
+            emergency_contact = form.cleaned_data('emergency_contact')
+            gender = form.cleaned_data('gender')
+            birthdate = form.cleaned_data('birthdate')
+            user = CustomUser(username='hana', email='hana@gmail.com', first_name=first_name, last_name=last_name, contact=contact, emergency_contact=emergency_contact, gender=gender, birthdate=birthdate)
+            print(user.first_name)
+            user.save()
             login(request, user)
-            return redirect('home')
+            return HttpResponseRedirect('home')
     else:
         form = CustomUserCreationForm()
-    return render(request, 'registration/signup.html', {'form': form})
+        return render(request, 'registration/signup.html', {'form': form})
 
 def vaccination_signup(request):
     return render(request, 'registration/vaccination.html')
