8e2099b4e4dfcc9497aac2e64f0c799ca6a17326
tiemfah <sivanat.sub@gmail.com>
2019-11-07 00:16:21 +0700

    mostly complete routing, need some cleanup

--- users/views.py
+++ users/views.py
@@ -1,39 +1,43 @@
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth import login, authenticate
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponseRedirect
 
 from .models import CustomUser
-from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .forms import  FormThatWork as MekInwRegisForm
 # Create your views here.
 
 
 class SignUpView(CreateView):
-    form_class = CustomUserCreationForm
+    form_class = MekInwRegisForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
 
 
 def signup(request):
     if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST, instance=request.user)
+        form = MekInwRegisForm(request.POST)
         user = CustomUser.objects.get(pk=request.user.pk)
-        first_name = form.cleaned_data.get('first_name')
-        last_name = form.cleaned_data.get('last_name')
-        contact = form.cleaned_data.get('contact')
-        emergency_contact = form.cleaned_data.get('emergency_contact')
-        gender = form.cleaned_data.get('gender')
-        birthdate = form.cleaned_data.get('birthdate')
-        user.update_profile(first_name=first_name,
-                            last_name=last_name,
-                            contact=contact,
-                            emergency_contact=emergency_contact,
-                            gender=gender,
-                            birthdate=birthdate)
-        return HttpResponseRedirect('home')
+        if form.is_valid():
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+            contact = form.cleaned_data.get('contact')
+            emergency_contact = form.cleaned_data.get('emergency_contact')
+            gender = form.cleaned_data.get('gender')
+            birthdate = form.cleaned_data.get('birthdate')
+
+            user.update_profile(first_name=first_name,
+                                last_name=last_name,
+                                contact=contact,
+                                emergency_contact=emergency_contact,
+                                gender=gender,
+                                birthdate=birthdate)
+            user.save()
+            # might change to redirect to vaccine page later.
+            return  HttpResponseRedirect(reverse('users:user', args=[request.user.pk]))
     else:
-        form = CustomUserCreationForm()
+        form = MekInwRegisForm()
         return render(request, 'registration/signup.html', {'form': form})
 
 
