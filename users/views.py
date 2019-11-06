411cf8e970f1aa4943f6659c3d10773b73f33bee
tiemfah <sivanat.sub@gmail.com>
2019-11-06 23:49:31 +0700

    making checkpoint before "newform" bomb drop

--- users/views.py
+++ users/views.py
@@ -17,28 +17,21 @@ class SignUpView(CreateView):
 
 def signup(request):
     if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST, instance=request.user)
         user = CustomUser.objects.get(pk=request.user.pk)
-        if form.is_valid():
-            first_name = form.cleaned_data.get('first_name')
-            print(first_name)
-            last_name = form.cleaned_data.get('last_name')
-            contact = form.cleaned_data.get('contact')
-            emergency_contact = form.cleaned_data.get('emergency_contact')
-            gender = form.cleaned_data.get('gender')
-            birthdate = form.cleaned_data.get('birthdate')
-<<<<<<< HEAD
-            user.update_profile(username='hana', email='hana@gmail.com', first_name=first_name, last_name=last_name,
-                                contact=contact, emergency_contact=emergency_contact, gender=gender, birthdate=birthdate)
-            # user.save()
-            # print('bahhhhhhhhhhhhhhhhh: ' + user.first_name)
-=======
-            
-            user = CustomUser(username='hana', email='hana@gmail.com', first_name=first_name, last_name=last_name, contact=contact, emergency_contact=emergency_contact, gender=gender, birthdate=birthdate)
-            user.save()
-            # login(request, user)
->>>>>>> 4152a99ccd55f2cf68fdceb0091da4c4d8657556
-            return HttpResponseRedirect('home')
+        first_name = form.cleaned_data.get('first_name')
+        last_name = form.cleaned_data.get('last_name')
+        contact = form.cleaned_data.get('contact')
+        emergency_contact = form.cleaned_data.get('emergency_contact')
+        gender = form.cleaned_data.get('gender')
+        birthdate = form.cleaned_data.get('birthdate')
+        user.update_profile(first_name=first_name,
+                            last_name=last_name,
+                            contact=contact,
+                            emergency_contact=emergency_contact,
+                            gender=gender,
+                            birthdate=birthdate)
+        return HttpResponseRedirect('home')
     else:
         form = CustomUserCreationForm()
         return render(request, 'registration/signup.html', {'form': form})
@@ -47,12 +40,8 @@ def signup(request):
 def vaccination_signup(request):
     return render(request, 'registration/vaccination.html')
 
+
 def user_view(request, user_id):
     user = CustomUser.objects.get(id=user_id)
-    print(dir(user))
-    context = {'user' : user}
-<<<<<<< HEAD
-    return render(request, 'test.html', context) 
-=======
+    context = {'user': user}
     return render(request, 'test.html', context)
->>>>>>> 4152a99ccd55f2cf68fdceb0091da4c4d8657556
