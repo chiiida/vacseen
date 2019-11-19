42c0acab65e1a645e79ab74e59cb227f43ebcef7
Sirikonss <s.sirikonss@gmail.com>
2019-11-19 15:03:41 +0700

    add test vaccine models

--- vaccine/tests.py
+++ vaccine/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
 
-# Create your tests here.
+from vaccine.models import VaccineModel, Vaccine, DoseModel, Dose
+
+class VaccineModelTest(TestCase):
+    
+    def test_string_representation(self):
+        vac = VaccineModel(vaccine_name="BCG")
+        self.assertEqual(str(vac),'Model: ' + vac.vaccine_name)
+
+
+class VaccineTest(TestCase):
+    
+    def test_string_representation(self):
+        vac = Vaccine(vaccine_name="Hepatitis A")
+        self.assertEqual(str(vac),vac.vaccine_name)
+
+
+class DoseModelTest(TestCase):
+
+    def test_string_representation(self):
+        vac = VaccineModel(vaccine_name="BCG")
+        dose = DoseModel(vaccine=vac, dose_count=2)
+        self.assertEqual(str(dose),f"{str(dose.vaccine)} : dose {dose.dose_count}" )
+
+# class Dose(TestCase):
+
+#     def test_string_representation(self):
+#         vac = Vaccine(vaccine_name="Hepatitis A")
+#         dose = Dose(vaccine=vac, dose_count=1)
+#         self.assertEqual(str(dose),f"{str(dose.vaccine)} : dose {dose.dose_count}" )
