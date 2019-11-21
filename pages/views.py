6fdc4e7e780a10dc3f452d2a41b03d80f9cf97a5
Sirikonss <s.sirikonss@gmail.com>
2019-11-21 13:30:04 +0700

    edit css

--- pages/views.py
+++ pages/views.py
@@ -14,10 +14,9 @@ def LoginHandler(request):
     else:
         return redirect('users:signup')
 
-def handler404(request, exception, template_name="404.html"):
-    response = render_to_response("404.html")
-    response.status_code = 404
-    return response
+
+def handler404(request, exception):
+    return render(request,'404.html', status = 404)
 
 def handler500(request, *args, **argv):
     response = render(request, '500.html', {})
