a34dbd329374dfe115bfc8cdc8b42021a0f56e9b
Chananchida F <chida.fua@gmail.com>
2019-11-24 19:38:57 +0700

    Test cases on pages/views.py

--- pages/tests/test_pages_views.py
+++ pages/tests/test_pages_views.py
@@ -1,24 +1,42 @@
-from django.test import TestCase, Client
+from django.test import TestCase, RequestFactory, Client
 from users.models import CustomUser
 from rest_framework.test import force_authenticate, APIClient
+from pages.views import *
 
 
 class PagesViewsTest(TestCase):
 
     def setUp(self):
-       self.client = APIClient()
+        self.request_factory = RequestFactory()
+        self.client = APIClient()
+        self.user = CustomUser.objects.create(first_name='User A')
+        self.user.save()
 
-    # def test_login_handler_without_signup(self):
-    #     user = CustomUser(first_name='User A')
-    #     self.client.force_authenticate(user=user)
-    #     response = self.client.get(path='/users/profile/', follow=True)
-    #     print(response)
-    #     self.assertRedirects(response, '/users/signup', status_code=200,
-    #                          target_status_code=200, fetch_redirect_response=True)
+    def test_login_handler_without_signup(self):
+        request = self.request_factory.get('/users/profile/')
+        request.user = self.user
+        self.client.force_authenticate(user=self.user)
+        response = LoginHandler(request)
+        response.client = self.client
+        self.assertRedirects(
+            response, '/users/signup/')
 
-        # with self.settings(LOGIN_URL='home'):
-        #     response = self.client.get('')
-        #     self.assertRedirects(response, '/users/signup', status_code=200)
+    def test_login_handler_already_signup(self):
+        user = CustomUser.objects.create(username='User',
+                                         first_name='User',
+                                         last_name='A',
+                                         contact='081764889',
+                                         emergency_contact='0878274444',
+                                         gender='Male',
+                                         birthdate='1999-07-30')
+        user.save()
+        request = self.request_factory.get('/users/profile/')
+        request.user = user
+        self.client.force_authenticate(user=user)
+        response = LoginHandler(request)
+        response.client = Client()
+        self.assertRedirects(
+            response, '/users/profile/', target_status_code=302)
 
     def test_404_handler(self):
         response = self.client.get(path='/users/signup/beebooo')
--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -7,30 +7,38 @@ from users.models import CustomUser
 from users.forms import CustomUserForm
 import datetime
 
-from django.test import TestCase, Client
-from users.views import signup, vaccination_signup
+from django.test import TestCase, RequestFactory, Client
+from rest_framework.test import force_authenticate, APIClient
+from users.views import *
 
-class ViewsTests(TestCase):
-    
+
+class ProfileViewTest(TestCase):
+
     def setUp(self):
-       self.client = Client()
+        self.user = CustomUser.objects.create(first_name='User A')
+        self.user.save()
+        self.request_factory = RequestFactory()
+        self.request = self.request_factory.get('/users/profile/')
+        self.request.user = self.user
+        self.client = APIClient()
+        self.response = user_view(self.request)
 
-    def test_index(self):
-        response = self.client.get(path='')
-        status = response.status_code
-        self.assertEqual(status, 200)
-    
-    def test_signup(self) :
-        response = self.client.get(path='/users/signup/')
-        status = response.status_code
-        self.assertEqual(status, 200)
-
-    def test_without_login_redirect(self):
-        response = self.client.get(path='/users/profile/', follow=True)
-        self.assertRedirects(response, '/?next=/users/profile/')
-
-    # def test_profile(self) :
-    #     c = Client()
-    #     response = c.get(path='/users/profile/')
+    # def test_index(self):
+    #     response = self.client.get(path='')
     #     status = response.status_code
     #     self.assertEqual(status, 200)
+
+    # def test_signup(self):
+    #     response = self.client.get(path='/users/signup/')
+    #     status = response.status_code
+    #     self.assertEqual(status, 200)
+
+    def test_user_view_without_login(self):
+        # self.assertTrue(200, self.response.status_code)
+        self.response.client = Client()
+        self.assertTemplateUsed(self.response, 'index.html')
+
+    def test_user_view(self):
+        self.client.force_authenticate(user=self.user)
+        # self.assertTrue(200, self.response.status_code)
+        self.assertContains(self.response, self.user.first_name)
