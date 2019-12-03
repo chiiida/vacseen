d57b4ab499a3a39c85807bebe298208bb91f6dea
tiemfah <sivanat.sub@gmail.com>
2019-12-03 10:38:09 +0700

    add parental view

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import signup_view, vaccination_signup_view, user_view
+from .views import signup_view, vaccination_signup_view, user_view, request_user_view
 
 app_name = 'users'
 urlpatterns = [
     path('profile/<int:user_id>/', user_view, name='profile'),
     path('signup/', signup_view, name='signup'),
     path('signup/vaccination/', vaccination_signup_view, name='vaccination'),
+    path('request_user/', request_user_view, name='request_user')
 ]
