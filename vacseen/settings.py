f16f755587eeef3c3d8bf0e401bbddfa26287f8a
tiemfah <sivanat.sub@gmail.com>
2019-12-12 12:15:47 +0700

    this is working correctly with debug=true

--- vacseen/settings.py
+++ vacseen/settings.py
@@ -1,6 +1,5 @@
 import os
 from decouple import config
-# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,5 +138,3 @@ LOGIN_REDIRECT_URL = 'loginhandler'  # new
 LOGOUT_REDIRECT_URL = 'home'  # new
 
 ACCOUNT_LOGOUT_ON_GET = True
-
-# django_heroku.settings(locals())
