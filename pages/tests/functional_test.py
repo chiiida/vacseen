c0410c8a6f483f4636cc822563103d57ea182611
Chananchida F <chida.fua@gmail.com>
2019-12-13 18:02:05 +0700

    Add E2E test for each app

--- pages/tests/functional_test.py
+++ pages/tests/functional_test.py
@@ -2,18 +2,22 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 
 
-class IndexFunctionalTest(LiveServerTestCase):
+class UserFunctionalTest(LiveServerTestCase):
+    username = 'testuser'
+    password = '12345678test'
 
     def setUp(self):
         options = webdriver.ChromeOptions()
         options.add_argument('--headless')
         self.browser = webdriver.Chrome(chrome_options=options)
-        super(IndexFunctionalTest, self).setUp()
+        super(UserFunctionalTest, self).setUp()
 
     def tearDown(self):
         self.browser.quit()
-        super(IndexFunctionalTest, self).tearDown()
+        super(UserFunctionalTest, self).tearDown()
 
     def test_click_login(self):
         self.browser.get(self.live_server_url)
         self.browser.find_element_by_class_name('google-button').click()
+        expected_url = self.live_server_url + '/accounts/google/login/'
+        self.assertEqual(self.browser.current_url, expected_url)
